c2109dbaf6873826d604796e0f51673ff80d71cc
chan <cham9994@gmail.com>
2022-06-16 16:53:05 +0900

    feat: API request

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,7 +5,11 @@ import HeroBannerImage from '../assets/images/banner.png'
 
 const HeroBanner = () => {
   return (
-    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }}>
+    <Box
+      sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }}
+      position="relative"
+      p="20px"
+    >
       <Typography color="#FF2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
@@ -32,14 +36,11 @@ const HeroBanner = () => {
         fontWeight={600}
         color="#ff2625"
         sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' } }}
+        fontSize="200px"
       >
         Exercise
       </Typography>
-      <img
-        src={HeroBannerImage}
-        alt="hero-banner"
-        className="hero-banner-img"
-      />
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   )
 }
